c63dda894c197858b00615a7552d840a1370b1cf
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-05-10 06:21:42 -0700

    reworked controller after running into errors

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,111 +1,97 @@
-const { User, Comment, reaction } = require('../models');
+const { Comment, User, Types } = require('../models');
 
-module.exports = {
-  // Function to get all of the commentss by invoking the find() method with no arguments.
-  // Then we return the results as JSON, and catch any errors. Errors are sent as JSON with a message and a 500 status code
-  getComments(req, res) {
-    Comment.find()
-      .then((comments) => res.json(comments))
-      .catch((err) => res.status(500).json(err));
-  },
-  // Gets a single comments using the findOneAndUpdate method. We pass in the ID of the comments and then respond with it, or an error if not found
-  getSinglecomment(req, res) {
-    Comment.findOne({ _id: req.params.commentId })
-      .then((comment) =>
-        !comment
-          ? res.status(404).json({ message: 'No comment with that ID' })
-          : res.json(comment)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Creates a new comments. Accepts a request body with the entire comments object.
-  // Because commentss are associated with Users, we then update the User who created the app and add the ID of the comments to the commentss array
-  createComment(req, res) {
-    Comment.create(req.body)
-      .then((comments) => {
-        return User.findOneAndUpdate(
-          { _id: req.body.userId },
-          { $addToSet: { comments: comment._id } },
-          { new: true }
-        );
+
+const CommentController = {
+    getAllComments(req, res) {
+        Comment.find({})
+            .select("-__v")
+            .sort({ _id: -1 })
+            .then((dbCommentData) => res.json(dbCommentData))
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    getCommentById({ params }, res) {
+        console.log("params sent", params)
+        Comment.findOne({ _id: params.CommentId })
+      .select("-__v")
+      .then((dbCommentData) => {
+        if (!dbCommentData) {
+          res.status(404).json({ message: "No Comment found with this id!" });
+          return;
+        }
+        res.json(dbCommentData);
       })
-      .then((user) =>
-        !user
-          ? res.status(404).json({
-              message: 'comment created, but found no user with that ID',
-            })
-          : res.json('Created the comments 🎉')
-      )
       .catch((err) => {
         console.log(err);
-        res.status(500).json(err);
+        res.status(400).json(err);
       });
-  },
-  // Updates and comments using the findOneAndUpdate method. Uses the ID, and the $set operator in mongodb to inject the request body. Enforces validation.
-  updateComment(req, res) {
-    Comment.findOneAndUpdate(
-      { _id: req.params.commentId },
-      { $set: req.body },
-      { runValidators: true, new: true }
-    )
-      .then((comments) =>
-        !comments
-          ? res.status(404).json({ message: 'No comments with this id!' })
-          : res.json(comments)
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
-  // Deletes a comment from the database. Looks for an app by ID.
-  // Then if the app exists, we look for any users associated with the app based on he app ID and update the commentss array for the User.
-  deleteComment(req, res) {
-    Comment.findOneAndRemove({ _id: req.params.commentId })
-      .then((comments) =>
-        !comments
-          ? res.status(404).json({ message: 'No comments with this id!' })
-          : User.findOneAndUpdate(
-              { comment: req.params.commentId },
-              { $pull: { comment: req.params.commentId } },
-              { new: true }
-            )
-      )
-      .then((user) =>
-        !user
-          ? res.status(404).json({
-              message: 'comments created but no user with this id!',
+    },
+    // add Comment to User
+    addComment({ params, body }, res) {
+        console.log("INCOMING BODY", body)
+        Comment.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { Comments: _id } },
+                    { new: true }
+                );
             })
-          : res.json({ message: 'comments successfully deleted!' })
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Adds a tag to an comments. This method is unique in that we add the entire body of the tag rather than the ID with the mongodb $addToSet operator.
-  addReaction(req, res) {
-    Comment.findOneAndUpdate(
-      { _id: req.params.commentId },
-      { $addToSet: { tags: req.body } },
-      { runValidators: true, new: true }
-    )
-      .then((comments) =>
-        !comments
-          ? res.status(404).json({ message: 'No comments with this id!' })
-          : res.json(comments)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // Remove comments tag. This method finds the comments based on ID. It then updates the tags array associated with the app in question by removing it's tagId from the tags array.
-  removeReaction(req, res) {
-    Comment.findOneAndUpdate(
-      { _id: req.params.commentId },
-      { $pull: { tags: { tagId: req.params.tagId } } },
-      { runValidators: true, new: true }
-    )
-      .then((comments) =>
-        !comments
-          ? res.status(404).json({ message: 'No comments with this id!' })
-          : res.json(comments)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id! first error' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    // remove Comment
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.CommentId })
+            .then(deletedComment => {
+                if (!deletedComment) {
+                    return res.status(404).json({ message: 'No Comment with this id!' });
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.username },
+                    { $pull: { Comments: params.CommentId } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    addReaction({ params, body }, res) {
+        console.log("INCOMING BODY", body)
+        Comment.findOneAndUpdate(
+            { _id: params.CommentId },
+            { $push: { reactions: body } },
+            { new: true }
+        )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    // remove reaction
+    removeReaction({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.CommentId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    }
 };
+
+module.exports = CommentController
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,117 @@
-const { User, Comment, reaction } = require('../models');
+const { User, Comment } = require('../models')
 
-module.exports = {
-  // Get all users
-  getUsers(req, res) {
-    User.find()
-      .then((users) => res.json(users))
-      .catch((err) => res.status(500).json(err));
-  },
-  // Get a single user
-  getSingleUser(req, res) {
-    User.findOne({ _id: req.params.userId })
-      .select('-__v')
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No user with that ID' })
-          : res.json(user)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // create a new user
-  createUser(req, res) {
-    User.create(req.body)
-      .then((user) => res.json(user))
-      .catch((err) => res.status(500).json(err));
-  },
-  // Delete a user and associated apps
-  deleteUser(req, res) {
-    User.findOneAndDelete({ _id: req.params.userId })
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: 'No user with that ID' })
-          : Application.deleteMany({ _id: { $in: user.applications } })
-      )
-      .then(() => res.json({ message: 'User and associated apps deleted!' }))
-      .catch((err) => res.status(500).json(err));
-  },
-};
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'Comments',
+                select: ('-__v')
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            })
+    },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+          .populate({
+            path: "Comments",
+            select: "-__v",
+          })
+          .select("-__v")
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
+    createUser({ body }, res) {
+        console.log("BODY OBJECT", body)
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err)
+                res.status(400).json(err)
+            })
+    },
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {
+          new: true,
+          runValidators: true,
+        })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.status(400).json(err));
+      },
+      deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id" });
+              return;
+            }
+            User.updateMany(
+              { _id: { $in: dbUserData.friends } },
+              { $pull: { friends: params.id } }
+            )
+              .then(() => {
+                Comment.deleteMany({ username: dbUserData.username })
+                  .then(() => {
+                    res.json({ message: "Successfully deleted user" });
+                  })
+                  .catch((err) => res.status(400).json(err));
+              })
+              .catch((err) => res.status(400).json(err));
+          })
+          .catch((err) => res.status(400).json(err));
+      },
+    addFriend({ params }, res) {
+        User.findByIdAndUpdate(
+          { _id: params.id },
+          { $addToSet: { friends: params.friendId } },
+          { new: true }
+        )
+          .select("-__v")
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      },
+      removeFriend({ params }, res) {
+        User.findByIdAndUpdate(
+          { _id: params.id },
+          { $pull: { friends: params.friendId } },
+          { new: true, runValidators: true }
+        )
+          .select("-__v")
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No friend found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.status(400).json(err));
+      },
+}
+
+module.exports = userController
